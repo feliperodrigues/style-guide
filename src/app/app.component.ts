ab6cc8bb4950a94a2d60f22711189d74478cfb44
Felipe Rodrigues <felipe.rodrigues.flex@gmail.com>
2017-07-06 09:48:36 -0300

    Initial Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { Http, Response }  from '@angular/http';
+import {Observable} from "rxjs/Observable";
+import 'rxjs/Rx';
+import {DomSanitizer} from "@angular/platform-browser";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+	htmlCode: string;
+
+	currentDoc: any;
+	htmlExample: string;
+
+	@ViewChild('codeView')
+	private codeViewer: ElementRef;
+
+	constructor(private http: Http, private sanitizer: DomSanitizer) {
+	}
+
+	ngOnInit() {
+		this.loadAlertDoc();
+	}
+
+	public loadButtonDoc() {
+		this.getButtonDoc().subscribe(
+			result => {
+				this.currentDoc = result;
+				this.currentDoc.example = this.sanitizer.bypassSecurityTrustHtml(result.example);
+				this.htmlExample = result.example;
+			},
+			error => {
+				console.error("Arquivo não encontrado");
+			}
+		)
+	}
+
+	public loadAlertDoc() {
+		this.getAlertDoc().subscribe(
+			result => {
+				this.currentDoc = result;
+				this.htmlExample = result.example;
+			},
+			error => {
+				console.error("Arquivo não encontrado");
+			}
+		)
+	}
+
+	private getButtonDoc() {
+		return this.http.get('./assets/doc/button.json')
+			.map(this.extractData)
+			.catch(this.handleError);
+	}
+
+	private getAlertDoc() {
+		return this.http.get('./assets/doc/alert.json')
+			.map(this.extractData)
+			.catch(this.handleError);
+	}
+
+	protected extractData(res: Response) {
+		let body = res.json();
+		return body || {};
+	}
+
+	protected handleError(error: Response) {
+		let body = error.json();
+
+		if (error.status == 400) {
+			//console.error(error);
+			//console.error(body.message);
+		}
+
+		return Observable.throw('ops');
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
+import { MarkdownModule } from 'angular2-markdown';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule,
+		MarkdownModule.forRoot(),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
